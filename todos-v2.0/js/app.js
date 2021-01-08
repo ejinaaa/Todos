66bf2c68593f98df7557834caf73b4ad9be3c3a1
ejinaaa <eunjink0423@gmail.com>
2021-01-09 01:35:24 +0900

    feat: Remake

--- todos-v2.0/js/app.js
+++ todos-v2.0/js/app.js
@@ -1,98 +1,107 @@
 const $inputTodo = document.querySelector('.input-todo');
 const $todos = document.querySelector('.todos');
 const $completeAll = document.querySelector('.complete-all');
-const $completeAllCheckbox = document.getElementById('ck-complete-all');
-const $removeCompletedBtn = document.querySelector('.clear-completed > .btn');
-const $completedCount = document.querySelector('.completed-todos');
-const $activeCount = document.querySelector('.active-todos');
+const $completeAllCheckbox = document.querySelector('.complete-all > .checkbox');
+const $completeTodosNumber = document.querySelector('.completed-todos');
+const $uncompleteTodosNumber = document.querySelector('.active-todos');
+const $removeCompletedTodosBtn = document.querySelector('.clear-completed > .btn')
 
 let todos = [];
 
-const render = () => {
-  $todos.innerHTML = todos.map(({ id, content, completed }) => `<li id="${id}" class="todo-item">
-      <input id="ch-${id}" class="checkbox" type="checkbox" ${completed ? 'checked' : ''}>
-      <label for="ck-${id}" style="color: ${completed ? 'lightgrey' : ''}; text-decoration: ${completed ? 'line-through' : ''}">${content}</label>
-      <i class="remove-todo far fa-times-circle"></i>
-    </li>`).join('');
-  
-  if (todos.length === todos.filter(({ completed }) => completed).length) {
-    $completeAllCheckbox.checked = true;
-  }
-  if (todos.length !== todos.filter(({ completed }) => completed).length) {
-    $completeAllCheckbox.checked = false;
-  }
+// Functions
+const countTodos = () => {
+  $completeTodosNumber.textContent = todos.filter(({ completed }) => completed).length;
+  $uncompleteTodosNumber.textContent = todos.filter(({ completed }) => !completed).length;
+};
 
-  $completedCount.textContent = todos.filter(({ completed }) => completed).length;
-  $activeCount.textContent = todos.filter(({ completed }) => !completed).length;
+const changeCompleteAllCheckbox = () => {
+  $completeAllCheckbox.checked = todos.filter(todo => todo.completed === true).length === todos.length ? true : false;
+};
+
+const render = () => {
+  $todos.innerHTML = todos.map(({ id, content, completed }) => {
+    return `<li id="${id}" class="todo-item">
+    <input id="ck-${id}" class="checkbox" type="checkbox" ${completed ? 'checked' : ''}>
+    <label for="ck-${id}">${content}</label>
+    <i class="remove-todo far fa-times-circle"></i>
+  </li>`
+  }).join('');
+
+  countTodos();
+
+  changeCompleteAllCheckbox();
 };
 
 const fetchTodo = () => {
   todos = [
     { id: 1, content: 'HTML', completed: true },
     { id: 2, content: 'CSS', completed: true },
-    { id: 3, content: 'JavaScript', completed: false }
+    { id: 3, content: 'JavaScript', completed: false },
   ];
-  todos.sort((todo1, todo2) => (todo1.id > todo2.id ? -1 : (todo1.id < todo2.id ? 1 : 0)));
+
+  todos = [...todos].sort((todo1, todo2) => todo1.id < todo2.id ? 1 : todo1.id > todo2.id ? -1 : 0);
 
   render();
 };
 
-const addTodo = (() => {
-  const newId = () => Math.max(...todos.map(({ id }) => id), 0);
-  return e => {
-  todos = [{ id: newId() + 1, content: e.target.value, completed: false }, ...todos];
-  render();
-  };
-})();
+const generateId = () => {
+  return Math.max(...todos.map(({ id }) => id), 0);
+};
 
-const removeTodo = targetId => {
-  todos = todos.filter(todo => +targetId !== todo.id);
+const addTodo = () => {
+  if (!$inputTodo.value) return;
+  todos = [{ id: generateId() + 1, content: $inputTodo.value, completed: false }, ...todos];
+
+  render();
+};
+
+const removeTodo = id => {
+  todos = todos.filter((todo) => !(todo.id === +id));
+
   render();
 };
 
 const toggleTodo = targetId => {
-  todos = todos.map(({ id, content, completed}) => ({ id,
-    content,
-    completed: +targetId === id ? !completed : completed }));
+  todos = todos.map(({ id, content, completed }) => ({id, content, completed: (id === +targetId ? !completed : completed)}));
+
   render();
 };
 
 const checkCompleteAll = () => {
-  if (!$completeAllCheckbox.checked) {
-    todos = todos.map(({ id, content }) => ({ id, content, completed: false }));
-  } else {
-    todos = todos.map(({ id, content }) => ({ id, content, completed: true }));
-  }
-  render();
-}
+  todos = todos.map(({ id, content, completed }) => ({ id, content, completed: $completeAllCheckbox.checked ? true : false}));
 
-const removeCompletedTodo = () => {
+  render();
+};
+
+const removeCompletedTodos = () => {
   todos = todos.filter(({ completed }) => !completed);
-  render();
-}
 
+  render();
+};
+
+// Events
 document.addEventListener('DOMContentLoaded', fetchTodo);
 
 $inputTodo.addEventListener('keyup', e => {
-  if (e.key !== 'Enter') return;
-  if (!$inputTodo.value) return;
-
-  addTodo(e);
+  if (!(e.key === 'Enter')) return;
+  addTodo();
   $inputTodo.value = '';
 });
 
 $todos.addEventListener('click', e => {
-  const targetId = e.target.parentNode.id;
-  if (e.target.matches('.todo-item > .remove-todo')) removeTodo(targetId);
-  if (e.target.matches('.todo-item > label')) toggleTodo(targetId);
+  if (!e.target.matches('.remove-todo')) return;
+  removeTodo(e.target.parentNode.id);
 });
 
-$completeAll.addEventListener('click', () => {
+$todos.addEventListener('change', e => {
+  toggleTodo(e.target.parentNode.id);
+});
+
+$completeAll.addEventListener('change', () => {
   checkCompleteAll();
 });
 
-$removeCompletedBtn.addEventListener('click', () => {
-  removeCompletedTodo();
+$removeCompletedTodosBtn.addEventListener('click', () => {
+  removeCompletedTodos();
+  $completeAllCheckbox.checked = false;
 });
-
-// 2021-01-02 23:30 ~ 2021-01-03 02:20 완성
